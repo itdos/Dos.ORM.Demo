6273ea83483f4728f574db8df9f9a05009fc90b5
ITdos <admin@itdos.com>
2016-11-25 12:42:35 +0800

    更新demo

--- 初级玩法demo之mvc简单三层/Business/Base/DB.cs
+++ 初级玩法demo之mvc简单三层/Business/Base/DB.cs
@@ -13,14 +13,14 @@ namespace Business
     public class Db
     {
         //public static readonly DbSession Context = new DbSession("MySqlConn");
-        public static readonly DbSession Context = new DbSession("SqlServerConn");
-        //public static readonly DbSession Context = new DbSession("SqliteConn");
+        //public static readonly DbSession Context = new DbSession("SqlServerConn");
+        public static readonly DbSession Context = new DbSession("SqliteConn");
         //public static readonly DbSession Context = new DbSession("AccessConn");
         //public static readonly DbSession Context = new DbSession("OracleConn");
         //public static readonly DbSession Context = new DbSession("PostgreSqlConn");
         static Db()
         {
-            Context.RegisterSqlLogger(delegate(string sql)
+            Context.RegisterSqlLogger(delegate (string sql)
             {
                 //在此可以记录sql日志
                 //写日志会影响性能，建议开发版本记录sql以便调试，发布正式版本不要记录
